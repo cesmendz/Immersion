432683237e1dc6a71f0074812b7362aab491e7ba
cesmendz <165924540+cesmendz@users.noreply.github.com>
2024-05-13 19:56:47 -0700

    person.cs

---
+++ person.cs
@@ -0,0 +1,9 @@
+namespace user
+{
+    public class person
+    {
+        public string user {  get; set; }
+        public string password { get; set; }
+        public string name { get; set; }
+    }
+}
